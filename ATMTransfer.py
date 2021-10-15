5348b4817a9e09893ac7f0dc7a89452dd7ff6cc2
Hocket Apersil <91352832+Hocket1@users.noreply.github.com>
2021-10-15 09:25:16 +0000

    Create ATMTransfer.py

---
+++ ATMTransfer.py
@@ -0,0 +1,11 @@
+def transfer(self,trans_amount):
+        if trans_amount > self.balance:
+            print("\033[31mInsufficient fund!")
+            print(f"\033[34mYour balance is \033[32mGHC {self.balance} \033[34monly.")
+            print("Try with lesser amount than balance.")
+            print()
+        else:
+            self.balance = self.balance - trans_amount
+            print(f"\033[32mGHC {trans_amount} Transfer successful!")
+            print("\033[34mCurrent account balance: \033[32mGHC", self.balance)
+            print()        
