6848bbb6984e2e49e890ab80a424ef20af8644b8
Annik Oye Bediako <91503020+Annik-code@users.noreply.github.com>
2021-10-15 02:04:10 -0700

    Update Withdraw.py

--- Withdraw.py
+++ Withdraw.py
@@ -4,11 +4,11 @@ def withdraw(self, amount):
         self.amount = amount
         if self.amount > self.balance:
             print("\033[31mInsufficient fund!")
-            print(f"\033[34mYour balance is \033[32mGHC{self.balance} \033[34monly.")
+            print(f"\033[34mYour balance is \033[32mGHC {self.balance} \033[34monly.")
             print("Try with lesser amount than balance.")
             print()
         else:
             self.balance = self.balance - self.amount
-            print(f"\033[32mGHC{amount} withdrawal successful!")
+            print(f"\033[32mGHC {amount} withdrawal successful!")
             print("\033[34mCurrent account balance: \033[32mGHC", self.balance)
             print()
